10b9c515b11786cea09e0df4617625e3c405f65e
skittu <kartiksinghal15@gmail.com>
2020-01-28 16:34:33 +0530

    error

--- src/main/java/com/demigod/Zeta_Forum/Question/QuestionService.java
+++ src/main/java/com/demigod/Zeta_Forum/Question/QuestionService.java
@@ -3,6 +3,7 @@ package com.demigod.Zeta_Forum.Question;
 
 import com.demigod.Zeta_Forum.Answer.Answer;
 
+import com.demigod.Zeta_Forum.User.UserRepository;
 import com.fasterxml.jackson.databind.deser.DataFormatReaders;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,45 +31,43 @@ public class QuestionService {
 
     @Autowired
     private QuestionRepository questionRepository;
-  
-   @Autowired
+
+    @Autowired
     private TagRepository tagRepository;
 
-   // Question functions
+    @Autowired
+    private UserRepository userRepository;
+
+    // Question functions
 
 
+    public Question addQuestion(QuestionPostedFrontend que, String userId) {
 
-
-    public Question addQuestion(QuestionPostedFrontend que,String userId) {
- 
 
         // storing question in repository
         UUID questionUUID = UUID.randomUUID();
         Date date = new Date();
-        Question questionToBeInserted=new Question(
+        Question questionToBeInserted = new Question(
                 questionUUID.toString(),
                 que.getQuestion().toLowerCase(),
-                date,date,
+                date, date,
                 userId);
         questionRepository.save(questionToBeInserted);
 
         // storing tags in repository
-        addTags(que,questionUUID.toString());
+        addTags(que, questionUUID.toString());
 
         return questionToBeInserted;
     }
 
 
+    public ReturnQuestion getAllQuestions(String searchBy, String searchString, List<String> tags, String userId, String sortBy, Integer searchOrder, Integer pageSize, Integer pageNumber) throws IOException {
 
-
-    public ReturnQuestion getAllQuestions(String searchBy,String searchString,List<String> tags,String userId,String sortBy,Integer searchOrder,Integer pageSize,Integer pageNumber) throws IOException {
-
-        Pageable page = PageRequest.of(pageNumber,pageSize, Sort.by(sortBy).descending());
+        Pageable page = PageRequest.of(pageNumber, pageSize, Sort.by(sortBy).descending());
         System.out.println(searchBy);
-        if(searchBy.equals("Text"))
-        {
+        if (searchBy.equals("Text")) {
             System.out.println(searchString);
-            String s="";
+            String s = "";
             try {
                 Pattern p = Pattern.compile("\\b(he|she|the|...)\\b\\s?");
                 Matcher m = p.matcher(searchString);
@@ -88,30 +87,22 @@ public class QuestionService {
                 List<String> l = questionRepository.SearchQuestion(finalString);
 
                 return buildList(questionRepository.findByQuestionIdIn(l, page));
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 return new ReturnQuestion();
             }
-        }
-        else if(searchBy.equals("Tag"))
-        {
+        } else if (searchBy.equals("Tag")) {
 
-            List<String> listOfQuestionId= tagRepository.findQuestionId(tags,tags.size());
+            List<String> listOfQuestionId = tagRepository.findQuestionId(tags, tags.size());
 
-                return  buildList(questionRepository.findByQuestionIdIn(listOfQuestionId,page));
+            return buildList(questionRepository.findByQuestionIdIn(listOfQuestionId, page));
 
-        }
-        else if(searchBy.equals("Author"))
-        {
+        } else if (searchBy.equals("Author")) {
 
             return buildList(questionRepository.findAllByUserId(userId, page));
 
-        }
-        else
-        {
+        } else {
 
-                return buildList(questionRepository.findAll(page));
+            return buildList(questionRepository.findAll(page));
 
         }
 
@@ -123,19 +114,19 @@ public class QuestionService {
 
         // updating question
         Date date = new Date();
-        Question questionToBeUpdated =  questionRepository.findById(questionId).get();
+        Question questionToBeUpdated = questionRepository.findById(questionId).get();
 
-            questionToBeUpdated.setQuestion(questionBody.getQuestion());
-            questionToBeUpdated.setUpdatedOn(date);
-            questionRepository.save(questionToBeUpdated);
+        questionToBeUpdated.setQuestion(questionBody.getQuestion());
+        questionToBeUpdated.setUpdatedOn(date);
+        questionRepository.save(questionToBeUpdated);
 
-            //deleting tags
-            deleteTags(questionId);
+        //deleting tags
+        deleteTags(questionId);
 
-            // adding tags
-            addTags(questionBody, questionId);
+        // adding tags
+        addTags(questionBody, questionId);
 
-            return questionRepository.findById(questionId).get();
+        return questionRepository.findById(questionId).get();
 
     }
 
@@ -144,16 +135,14 @@ public class QuestionService {
 
         try {
 
-        // deleteing questions
-        questionRepository.deleteByQuestionId(questionId);
+            // deleteing questions
+            questionRepository.deleteByQuestionId(questionId);
 
-        // deleting tags
-        deleteTags(questionId);
+            // deleting tags
+            deleteTags(questionId);
 
-        return "Successfully deleted";
-        }
-        catch(Exception e)
-        {
+            return "Successfully deleted";
+        } catch (Exception e) {
             return "Some error occurred";
         }
 
@@ -161,42 +150,39 @@ public class QuestionService {
 
     // Tags functions
 
-    public void addTags(QuestionPostedFrontend que, String questionUUID)
-    {
+    public void addTags(QuestionPostedFrontend que, String questionUUID) {
         List<Tag> tagList = que.getQuestionTags().stream()
-                .map(t -> new Tag(t , questionUUID.toString())).collect(Collectors.toList());
+                .map(t -> new Tag(t, questionUUID.toString())).collect(Collectors.toList());
         tagRepository.saveAll(tagList);
     }
 
-    public void deleteTags(String questionId)
-    {
+    public void deleteTags(String questionId) {
         long recordsDeleted = tagRepository.deleteByQuestionId(questionId);
     }
 
-    public List<String> getTags(String questionId)
-    {
+    public List<String> getTags(String questionId) {
 
         return tagRepository.findAllByQuestionId(questionId).stream().map(t -> t.getTagName()).collect(Collectors.toList());
 
     }
 
 
-    public ReturnQuestion buildList(Page<Question> page)
-    {
+    public ReturnQuestion buildList(Page<Question> page) {
 
         ReturnQuestion returnQuestion = new ReturnQuestion();
         returnQuestion.setNumberOfElements(page.getNumberOfElements());
         returnQuestion.setTotalElements(page.getTotalElements());
         returnQuestion.setTotalPages(page.getTotalPages());
         returnQuestion.setQuestions(page.getContent());
-        List<List<String> > tags = new ArrayList<>();
+        List<List<String>> tags = new ArrayList<>();
+        List<String> userName = new ArrayList<>();
 
-        for(int i=0;i<page.getContent().size();i++)
-        {
+        for (int i = 0; i < page.getContent().size(); i++) {
             tags.add(getTags(page.getContent().get(i).getQuestionId()));
+           userName.add(userRepository.findById(page.getContent().get(i).getUserId()).get().getUserName());
         }
+        returnQuestion.setUserName(userName);
         returnQuestion.setTags(tags);
-
 
 
         return returnQuestion;
@@ -206,11 +192,12 @@ public class QuestionService {
     public Optional<Question> getSingleQuestion(String questionId) {
         return questionRepository.findById(questionId);
     }
-  public List<String> loadstop() throws IOException
-    {
+
+    public List<String> loadstop() throws IOException {
         List<String> stopwrods = Files.readAllLines(Paths.get("english_stopwords.txt"));
-        for(int i=0;i<10;i++)
+        for (int i = 0; i < 10; i++)
             System.out.println(stopwrods.get(i));
         return stopwrods;
 
     }
+}
